1eb1db7d7ad22fc8f1390757129dc2949fb2a416
BurakEmreGundes <63010381+BurakEmreGundes@users.noreply.github.com>
2021-09-09 12:51:04 +0300

    Marka controller güncellendi
    Marka controller işlemlerindeki eksikler tamamlandı.

--- source-code/controllers/brands.js
+++ source-code/controllers/brands.js
@@ -7,8 +7,30 @@ const ErrorResponse=require("../utils/errorResponse")
 // @route GET /api/v1/brands
 // @access Public
 exports.getBrands=asyncHandler(async ()=>{
-    res.status(200).json(res.advancedResults)
+    if(req.params.userId){
+       const brands=await Brand.find({user:req.params.userId}).populate({
+            path:"user",
+            select:"name role"
+        })
+        return res.status(200).json({success:true,data:brands})
+    }else{
+       return res.status(200).json(res.advancedResults)
+    }
+    
 })
+
+// @desc Create brand
+// @action POST /api/v1/brands
+// @access Private
+exports.addBrand=asyncHandler(async ()=>{
+    // Create
+    const brand=await Brand.create(req.body)
+
+    res.status(201).json({ success:true,data:brand })
+})
+
+
+
 
 
 // @desc Update Brand
@@ -34,3 +56,26 @@ exports.updateBrand=asyncHandler(async ()=>{
     
 })
 
+
+
+// @desc Delete Brand
+// @action DELETE /api/v1/brands/:brandId
+// @access Private
+exports.deleteBrand=asyncHandler(async(req,res,next)=>{
+    const brand=await Brand.findById(req.params.brandId);
+
+    // Check brand
+    if(!brand){
+        return next(new ErrorResponse(`Brand not found with the id of $${req.params.brandId}`,404));
+    }
+
+    // Ma
+    if(req.user._id!==brand.user.toString() || req.user.role!=="admin"){
+        return next(new ErrorResponse("",401))
+    }
+    
+    // Delete brand 
+    await brand.remove();
+
+    res.status(200).json({success:true,data:brand})
+})
--- source-code/server.js
+++ source-code/server.js
@@ -15,7 +15,7 @@ const PORT=process.env.PORT || 5000
 
 // Routers
 const brandRouter=require("./routes/brands")
-
+const authRouter=require("./routes/auth")
 
 
 
@@ -31,6 +31,7 @@ if(process.env.NODE_ENV === "development"){
 
 // Mount routes
 app.use("/api/v1/brands",brandRouter)
+app.use("/api/v1/auth",authRouter)
 
 
 // Listening to the server and log this runtime mode on console
